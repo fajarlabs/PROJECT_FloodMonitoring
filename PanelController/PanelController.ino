33ce14bea0ef571686cadb8e909ff247e9100087
fajarrdp <fajarrdp@gmail.com>
2021-05-02 12:58:48 +0700

    add library

--- PanelController/PanelController.ino
+++ PanelController/PanelController.ino
@@ -8,6 +8,9 @@
 #include <Filters.h>
 #define voltageSensorPin A0 // input voltage sensor
 #define voltageDCSensorPin A1
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 float testFrequency = 50; // Standard current freqwensi indonesia (Hz)
 float windowLength = 40.0/testFrequency;    
